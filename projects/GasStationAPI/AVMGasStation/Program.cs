a02b50adf6ef04c676a21839d5d0ed1b72b5f52a
Ludovit Scholtz <ludko2@gmail.com>
2025-04-19 13:48:01 +0200

    init the GasStation singleton during startup

--- projects/GasStationAPI/AVMGasStation/Program.cs
+++ projects/GasStationAPI/AVMGasStation/Program.cs
@@ -1,56 +1,60 @@
 
 using AlgorandAuthenticationV2;
+using AVMGasStation.BusinessControllers;
 using AVMGasStation.Model;
 
 namespace AVMGasStation
 {
-    public class Program
+  public class Program
+  {
+    public static void Main(string[] args)
     {
-        public static void Main(string[] args)
-        {
-            var builder = WebApplication.CreateBuilder(args);
+      var builder = WebApplication.CreateBuilder(args);
 
-            // Add services to the container.
+      // Add services to the container.
 
-            builder.Services.AddControllers();
-            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-            builder.Services.AddEndpointsApiExplorer();
-            builder.Services.AddSwaggerGen();
+      builder.Services.AddControllers();
+      // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+      builder.Services.AddEndpointsApiExplorer();
+      builder.Services.AddSwaggerGen();
 
-            builder.Services.Configure<AlgorandAuthenticationOptionsV2>(builder.Configuration.GetSection("AlgorandAuthentication"));
-            builder.Services.Configure<GasStationConfiguration>(builder.Configuration.GetSection("GasStation"));
+      builder.Services.Configure<AlgorandAuthenticationOptionsV2>(builder.Configuration.GetSection("AlgorandAuthentication"));
+      builder.Services.Configure<GasStationConfiguration>(builder.Configuration.GetSection("GasStation"));
+      builder.Services.AddSingleton<GasStation>();
 
-            var authOptions = builder.Configuration.GetSection("AlgorandAuthentication").Get<AlgorandAuthenticationOptionsV2>();
-            if (authOptions == null) throw new Exception("Config for the authentication is missing");
-            builder.Services.AddAuthentication(AlgorandAuthenticationHandlerV2.ID).AddAlgorand(a =>
-            {
-                a.Realm = authOptions.Realm;
-                a.CheckExpiration = authOptions.CheckExpiration;
-                a.EmptySuccessOnFailure = authOptions.EmptySuccessOnFailure;
-                a.AllowedNetworks = authOptions.AllowedNetworks;
-                a.Debug = authOptions.Debug;
-            });
+      var authOptions = builder.Configuration.GetSection("AlgorandAuthentication").Get<AlgorandAuthenticationOptionsV2>();
+      if (authOptions == null) throw new Exception("Config for the authentication is missing");
+      builder.Services.AddAuthentication(AlgorandAuthenticationHandlerV2.ID).AddAlgorand(a =>
+      {
+        a.Realm = authOptions.Realm;
+        a.CheckExpiration = authOptions.CheckExpiration;
+        a.EmptySuccessOnFailure = authOptions.EmptySuccessOnFailure;
+        a.AllowedNetworks = authOptions.AllowedNetworks;
+        a.Debug = authOptions.Debug;
+      });
 
-            builder.Services.AddProblemDetails();
+      builder.Services.AddProblemDetails();
 
-            var app = builder.Build();
+      var app = builder.Build();
 
-            // Configure the HTTP request pipeline.
-            if (app.Environment.IsDevelopment())
-            {
-                app.UseSwagger();
-                app.UseSwaggerUI();
-            }
-            
-            app.UseHttpsRedirection();
+      // Configure the HTTP request pipeline.
+      if (app.Environment.IsDevelopment())
+      {
+        app.UseSwagger();
+        app.UseSwaggerUI();
+      }
 
-            app.UseAuthentication();
-            app.UseAuthorization();
+      app.UseHttpsRedirection();
+
+      app.UseAuthentication();
+      app.UseAuthorization();
 
 
-            app.MapControllers();
+      app.MapControllers();
 
-            app.Run();
-        }
+      app.Services.GetService<GasStation>();
+
+      app.Run();
     }
+  }
 }
