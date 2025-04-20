54e305426edc81aa06cc6260a7fba20f861b5576
Scholtz <ludko2@gmail.com>
2025-04-20 19:03:11 +0200

    fe cicd

--- k8s/main/update-config.sh
+++ k8s/main/update-config.sh
@@ -1,3 +1,10 @@
+kubectl apply -f deployment-fe.yaml -n biatec-gas-station
+kubectl delete configmap gas-station-fe-main-conf -n biatec-gas-station
+kubectl create configmap gas-station-fe-main-conf --from-file=conf-fe -n biatec-gas-station
+kubectl rollout restart deployment/gas-station-fe-app-deployment -n biatec-gas-station
+kubectl rollout status deployment/gas-station-fe-app-deployment -n biatec-gas-station
+
+
 kubectl apply -f deployment-api.yaml -n biatec-gas-station
 kubectl delete configmap gas-station-api-main-conf -n biatec-gas-station
 kubectl create configmap gas-station-api-main-conf --from-file=conf-api -n biatec-gas-station
